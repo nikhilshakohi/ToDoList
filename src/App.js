56577571d39bb668c7b62aae264aeb002300bd95
Nikhil <nshakohi4@gmail.com>
2022-03-16 22:05:03 +0530

    Firebase Basic Deployed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+          <h1>Hello World!</h1>
+          <h3>Let's get started!!!</h3>
     </div>
   );
 }
